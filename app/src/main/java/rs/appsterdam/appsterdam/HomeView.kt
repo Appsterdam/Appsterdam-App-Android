03785f1a3f1076d5b89d6026c21de3a4ca5ca3fe
Wesley de Groot <email@wesleydegroot.nl>
2022-06-12 19:16:32 +0200

    Loading JSON

--- app/src/main/java/rs/appsterdam/appsterdam/HomeView.kt
+++ app/src/main/java/rs/appsterdam/appsterdam/HomeView.kt
@@ -6,10 +6,24 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import dev.jeziellago.compose.markdowntext.MarkdownText
 import rs.appsterdam.appsterdam.ui.theme.AppsterdamTheme
+import java.net.URL
+import java.io.File
+import org.json.JSONObject
+import kotlinx.coroutines.*
 
 class HomeView {
     init {
         println("HomeView")
+        GlobalScope.launch(Dispatchers.IO) {
+
+            val str = URL("https://appsterdam.rs/api/app.json").readText()
+          val json = JSONObject(str)
+
+          val home = json.getString("home")
+            // How to get the home var in MarkdownText
+            // or update MarkdownText
+          println("Home=" + home)
+        }
     }
 
     @Composable
@@ -22,11 +36,11 @@ class HomeView {
                 # Test markdown
                 This is a test
                 and this should work
-                
-                # Sample  
-                * Markdown  
-                * [Link](https://example.com)  
-                <a href="https://www.google.com/">Google</a> 
+
+                # Sample
+                * Markdown
+                * [Link](https://example.com)
+                <a href="https://www.google.com/">Google</a>
                 """.trimIndent()
             )
         }
