d1703b190538485850d2ff19c4fae18e8b2c4851
Wesley de Groot <email@wesleydegroot.nl>
2023-05-05 14:15:58 +0200

    Added icons for tabbar

--- app/src/main/java/rs/appsterdam/app/MainActivity.kt
+++ app/src/main/java/rs/appsterdam/app/MainActivity.kt
@@ -3,18 +3,31 @@ package rs.appsterdam.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Icon
 import androidx.compose.material.Tab
 import androidx.compose.material.TabRow
+import androidx.compose.material.icons.*
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.Check
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.Info
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.GlobalContext
 import org.koin.core.context.startKoin
@@ -71,6 +84,13 @@ fun TabView(showBottomSheet: (sheet: @Composable (() -> Unit) -> Unit) -> Unit) 
         "About"
     )
 
+    val tabIcons = listOf<Painter>( // cannot use this?
+        painterResource(R.drawable.home),
+        painterResource(R.drawable.events),
+        painterResource(R.drawable.jobs),
+        painterResource(R.drawable.info)
+    )
+
     Column {
         Column(
             modifier = Modifier
@@ -97,7 +117,10 @@ fun TabView(showBottomSheet: (sheet: @Composable (() -> Unit) -> Unit) -> Unit) 
                         tabIndex = index
                     },
                     icon = {
-                        Text("Icon")
+                        Icon(
+                            painter = tabIcons[index],
+                            contentDescription = title
+                        )
                     },
                     text = {
                         Text(text = title)
