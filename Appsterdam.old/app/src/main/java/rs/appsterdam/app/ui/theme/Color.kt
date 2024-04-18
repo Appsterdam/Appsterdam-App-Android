834e57af291bc529e10ce9c8e8efe03dd4193d3d
Wesley de Groot <email@wesleydegroot.nl>
2024-04-18 14:45:45 +0200

    Renamed `.old` to `.jetpack`.

--- Appsterdam.old/app/src/main/java/rs/appsterdam/app/ui/theme/Color.kt
+++
@@ -1,7 +0,0 @@
-package rs.appsterdam.app.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val AppsterdamPrimary = Color(0xFFFF0000)
-val AppsterdamSecondary = Color(0xFFFF0000)
-val BackgroundSecondary = Color(0xFFEFEFF4)
